698e76585a168650cb5142a14f482bb765fca814
unknown <sdgerbich@gmail.com>
2020-04-23 16:37:51 -0400

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,60 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 var momenttz = require("moment-timezone");
-var zipcode_to_timezone = require( 'zipcode-to-timezone' );
+var zipcode_to_timezone = require("zipcode-to-timezone");
 
 class App extends Component {
+  state = {};
+  // constructor(){
+  //   super();
+  //   state = {  }
 
+  // }
+  getTime = () => {
+    var zip = document.getElementById("userinput").value;
+    var realZip = parseInt(zip);
+    var zone = zipcode_to_timezone.lookup(realZip);
+    var time = momenttz().tz(zone).format("h:mm:ss a");
+    console.log(time);
+    console.log();
+  };
 
+  getWeather = () => {
+    var zip = document.getElementById("userinput").value;
+    var realZip = parseInt(zip);
+    console.log(process.env.REACT_APP_API_KEY);
+    if (zip.length == 5 && typeof realZip === "number") {
+      var weather =
+        "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?zip=" +
+        realZip +
+        "&appid=" +
+        process.env.REACT_APP_API_KEY;
+      console.log(weather);
+      fetch(weather)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          this.setState({weather: data.main.temp});
+          
+        });
+    }else{
+      console.log("thats not a number bro")
+    }
+  };
 
-constructor(){
-  super();
-  state = {  }
-
-}
-  
-
-
-  render() { 
-    return (  
-      <div>
+  render() {
+    return (
+      <>
         <input type="text" id="userinput" />
-
-
-      </div>
+        <button onClick={this.getWeather}>GO!</button>
+        <div>
+          <span id="temp"></span>
+        </div>
+      </>
     );
   }
 }
- 
+
 export default App;
