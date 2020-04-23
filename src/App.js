68bb2bb243ce75a370e186c39ec820ab9f91049b
unknown <sdgerbich@gmail.com>
2020-04-23 13:38:39 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+var momenttz = require("moment-timezone");
+var zipcode_to_timezone = require( 'zipcode-to-timezone' );
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+
+constructor(){
+  super();
+  state = {  }
+
 }
+  
 
+
+  render() { 
+    return (  
+      <div>
+        <input type="text" id="userinput" />
+
+
+      </div>
+    );
+  }
+}
+ 
 export default App;
