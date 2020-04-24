886258f56281ab6711294b354ee4cb388e33ed31
unknown <sdgerbich@gmail.com>
2020-04-24 14:27:18 -0400

    functioning

--- src/App.js
+++ src/App.js
@@ -15,8 +15,10 @@ class App extends Component {
     var realZip = parseInt(zip);
     var zone = zipcode_to_timezone.lookup(realZip);
     var time = momenttz().tz(zone).format("h:mm:ss a");
-    console.log(time);
-    console.log();
+    this.setState({
+      time: time
+    })
+    console.log(this.state);
   };
 
   getWeather = () => {
@@ -28,7 +30,8 @@ class App extends Component {
         "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?zip=" +
         realZip +
         "&appid=" +
-        process.env.REACT_APP_API_KEY;
+        process.env.REACT_APP_API_KEY +
+        "&units=imperial";
       console.log(weather);
       fetch(weather)
         .then((response) => {
@@ -36,8 +39,11 @@ class App extends Component {
         })
         .then((data) => {
           console.log(data);
-          this.setState({weather: data.main.temp});
-          
+          this.setState({
+            weather: data.main.temp + " degrees ",
+            place: data.name
+          });
+          this.getTime();
         });
     }else{
       console.log("thats not a number bro")
@@ -50,7 +56,11 @@ class App extends Component {
         <input type="text" id="userinput" />
         <button onClick={this.getWeather}>GO!</button>
         <div>
-          <span id="temp"></span>
+        <span id="place">Place: {this.state.place}</span>
+        <br></br>
+        <span id="time">Time: {this.state.time}</span>
+        <br></br>
+        <span id="weather">Weather: {this.state.weather}</span>
         </div>
       </>
     );
